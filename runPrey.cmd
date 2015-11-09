20738b276b94e00729bde136879c49ccdaa9d927
mullachv <mullachv@gmail.com>
2015-11-09 01:08:52 -0500

    .cmd files

--- runPrey.cmd
+++ runPrey.cmd
@@ -1,6 +1,3 @@
-rem !/bin/bash
-rem CLASSPATH=/Users/vmullachery/workspace/brooklyn-omniheurists/evasion/evasion/target/evasion-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-rem CLASSPATH=${CLASSPATH}:/Users/vmullachery/workspace/brooklyn-omniheurists/javax.websocket-api-1.1.jar
-set CLASSPATH=.\lib\javax.websocket-api-1.1.jar;.\lib\evasion-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+set BOOTPATH=.\lib\javax.websocket-api-1.1.jar;.\lib\json-simple-1.1.1.jar:.\lib\json.jar:.\lib\jetty-all-9.0.4.v20130625.jar:.\lib\jackson-core-2.5.0.jar:.\lib\jackson-databind-2.5.4.jar:.\lib\jackson-annotations-2.5.0.jar
 
-java -Xbootclasspath/a;"%CLASSPATH%" -jar .\lib\prey.jar
+java -Xbootclasspath/a:"%BOOTPATH%" -jar .\lib\prey.jar
